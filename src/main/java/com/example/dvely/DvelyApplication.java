504efa6bb8a01790da1710e979e07f728165cce3
Lee UnHak <uhlee@cyglobal.net>
2026-03-31 23:29:50 +0900

    chore: spring boot basic setup

---
+++ src/main/java/com/example/dvely/DvelyApplication.java
@@ -0,0 +1,13 @@
+package com.example.dvely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dvely/DvelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dvely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
